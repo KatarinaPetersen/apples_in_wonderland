a1325211c6ac2639267fe74fe06562ffe711c818
KatarinaPetersen <8555@rts-365.dk>
2017-11-03 07:40:06 +0100

    seneste rettelser

---
+++ public/assets/js/infobox.js
@@ -0,0 +1,73 @@
+addEventListener("DOMContentLoaded", function (event) {
+    document.querySelector("#colorBox1").addEventListener("click", function (event) {
+        document.querySelector("#content").style.backgroundColor = "#033944";
+        document.querySelector("#content").innerHTML = ` <div class="col-xs-6">
+        <h3>Lorem ipsum</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel sodales dui. Praesent pellentesque
+            risus a tempor condimentum. Nullam pulvinar tristique enim, in fringilla neque sagittis ac. Nullam
+            congue facilisis erat, in scelerisque lorem faucibus sit amet. Donec blandit maximus lorem. Nunc
+            sit amet lacus nunc. Donec libero arcu, rhoncus nec tempor sed, accumsan a justo. Aliquam quis tincidunt
+            erat. Phasellus in mi libero. Vivamus finibus convallis libero eu sagittis. Sed et fermentum diam.
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam dapibus
+            erat non lectus maximus, vel tempor mi scelerisque. Vestibulum ante ipsum primis in faucibus orci
+            luctus et ultrices posuere cubilia Curae; Phasellus ut ante molestie, rhoncus ante at, dignissim
+            nibh. Ut et ex lobortis, iaculis lacus et, rhoncus tortor. Phasellus nec nunc vel nulla semper fermentum.
+            Sed et lectus augue. Aliquam semper molestie erat eu varius. Suspendisse potenti. Mauris vel lacus
+            eget nisl facilisis iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. Integer non finibus mauris. Pellentesque risus mauris, placerat id orci et, scelerisque
+            euismod nibh. Proin elementum pretium accumsan. Curabitur vel eros eu turpis tristique condimentum
+            vel at nisl. Curabitur vestibulum id metus at lobortis. Nulla porta sagittis nulla et venenatis.</p>
+    </div>
+    <div class="col-xs-4 col-xs-offset-2">
+        <img class="img-responsive" src="./assets/img/applesstickbow.jpg">
+    </div>`;
+    })
+    document.querySelector("#colorBox2").addEventListener("click", function (event) {
+        document.querySelector("#content").style.backgroundColor = "#4d9099";
+        document.querySelector("#content").innerHTML = ` 
+        <div class="col-xs-4">
+        <img class="img-responsive" src="./assets/img/candyapplesdrip.jpg">
+        </div>
+        <div class="col-xs-6 col-xs-offset-2">
+        <h3>Lorem ipsum</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel sodales dui. Praesent pellentesque
+            risus a tempor condimentum. Nullam pulvinar tristique enim, in fringilla neque sagittis ac. Nullam
+            congue facilisis erat, in scelerisque lorem faucibus sit amet. Donec blandit maximus lorem. Nunc
+            sit amet lacus nunc. Donec libero arcu, rhoncus nec tempor sed, accumsan a justo. Aliquam quis tincidunt
+            erat. Phasellus in mi libero. Vivamus finibus convallis libero eu sagittis. Sed et fermentum diam.
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam dapibus
+            erat non lectus maximus, vel tempor mi scelerisque. Vestibulum ante ipsum primis in faucibus orci
+            luctus et ultrices posuere cubilia Curae; Phasellus ut ante molestie, rhoncus ante at, dignissim
+            nibh. Ut et ex lobortis, iaculis lacus et, rhoncus tortor. Phasellus nec nunc vel nulla semper fermentum.
+            Sed et lectus augue. Aliquam semper molestie erat eu varius. Suspendisse potenti. Mauris vel lacus
+            eget nisl facilisis iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. Integer non finibus mauris. Pellentesque risus mauris, placerat id orci et, scelerisque
+            euismod nibh. Proin elementum pretium accumsan. Curabitur vel eros eu turpis tristique condimentum
+            vel at nisl. Curabitur vestibulum id metus at lobortis. Nulla porta sagittis nulla et venenatis.</p>
+    </div>
+`;
+    })
+    document.querySelector("#colorBox3").addEventListener("click", function (event) {
+        document.querySelector("#content").style.backgroundColor = "#b93035";
+        document.querySelector("#content").innerHTML = ` <div class="col-xs-6">
+        <h3>Lorem ipsum</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel sodales dui. Praesent pellentesque
+            risus a tempor condimentum. Nullam pulvinar tristique enim, in fringilla neque sagittis ac. Nullam
+            congue facilisis erat, in scelerisque lorem faucibus sit amet. Donec blandit maximus lorem. Nunc
+            sit amet lacus nunc. Donec libero arcu, rhoncus nec tempor sed, accumsan a justo. Aliquam quis tincidunt
+            erat. Phasellus in mi libero. Vivamus finibus convallis libero eu sagittis. Sed et fermentum diam.
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam dapibus
+            erat non lectus maximus, vel tempor mi scelerisque. Vestibulum ante ipsum primis in faucibus orci
+            luctus et ultrices posuere cubilia Curae; Phasellus ut ante molestie, rhoncus ante at, dignissim
+            nibh. Ut et ex lobortis, iaculis lacus et, rhoncus tortor. Phasellus nec nunc vel nulla semper fermentum.
+            Sed et lectus augue. Aliquam semper molestie erat eu varius. Suspendisse potenti. Mauris vel lacus
+            eget nisl facilisis iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. Integer non finibus mauris. Pellentesque risus mauris, placerat id orci et, scelerisque
+            euismod nibh. Proin elementum pretium accumsan. Curabitur vel eros eu turpis tristique condimentum
+            vel at nisl. Curabitur vestibulum id metus at lobortis. Nulla porta sagittis nulla et venenatis.</p>
+    </div>
+    <div class="col-xs-4 col-xs-offset-2">
+        <img class="img-responsive" src="./assets/img/candyapplesstick.jpg">
+    </div>`;
+    })
+})
